7380cd375785540a5d1089587159218f107886ac
GeorgeCoblisan <geocoblisan@gmail.com>
2025-03-10 18:22:11 +0200

    initialize project

---
+++ src/main/java/com/safe/springboot/api/safe_bite/SafeBiteApplication.java
@@ -0,0 +1,13 @@
+package com.safe.springboot.api.safe_bite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeBiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeBiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safe/springboot/api/safe_bite/SafeBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safe.springboot.api.safe_bite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeBiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
