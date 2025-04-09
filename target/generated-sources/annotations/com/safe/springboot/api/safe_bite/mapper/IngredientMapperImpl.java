721bab99b5b5c6372dcc778fd1ffd7a668a53dc2
GeorgeCoblisan <geocoblisan@gmail.com>
2025-04-09 20:50:40 +0300

    add security based on amazon cognito and bearer tokens

--- target/generated-sources/annotations/com/safe/springboot/api/safe_bite/mapper/IngredientMapperImpl.java
+++ target/generated-sources/annotations/com/safe/springboot/api/safe_bite/mapper/IngredientMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-25T19:17:23+0200",
+    date = "2025-04-09T19:37:12+0300",
     comments = "version: 1.6.0.Beta1, compiler: javac, environment: Java 23.0.2 (Oracle Corporation)"
 )
 @Component
